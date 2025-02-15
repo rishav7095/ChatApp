fe2a4b2e15664a3db2b4d0a46b23748fe5782d44
Rishav Kumar <rishavkumar7095@example.com>
2025-02-15 23:30:22 +0530

    url update

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chatapp-0pm4.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
